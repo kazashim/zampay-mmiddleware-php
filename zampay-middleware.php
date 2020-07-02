d03a2958ec77cb103e8b08a0e13f1c154f2c8906
kazashim <kazashimkuzasuwat@gmail.com>
2020-07-02 23:50:47 +0200

    update

--- zampay-middleware.php
+++ zampay-middleware.php
@@ -1,7 +1,10 @@
 <?php
-
+//Api User
+$ConversationID = 'Marchant conversation ID';
+$ThirdPartyID = 'ThirdPartyID';
 $Password = 'marchant  password';
 $Shortcode = 'marchant shortcode';
 
+$zampay_api_user = $ConversationID . ':' . $ThirdPartyID . ':' .  $Password . ':' .  $Shortcode;
 
 ?>
